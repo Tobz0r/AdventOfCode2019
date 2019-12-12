169815a6584c4650016dfeda13eabda2f33e944d
Tobias Estefors <tobias.estefors@tieto.com>
2019-12-12 13:12:55 +0100

    day6

--- 6/day6.py
+++ 6/day6.py
@@ -15,6 +15,7 @@ def main():
     """main"""
     orbit_map = generate_orbit_map(open("input.txt").read().splitlines())
     print("Part 1: ", sum(len(find_path(orbit_map, planet)) for planet in orbit_map))
-    print("Part 2: ", len(set(find_path(orbit_map, "YOU")) ^ set(find_path(orbit_map, "SAN"))))
+    print("Part 2: ", len(set(find_path(orbit_map, "YOU")).symmetric_difference(set(find_path(orbit_map, "SAN")))))
+
 
 main()
