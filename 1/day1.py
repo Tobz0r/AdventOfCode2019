b80370b8d6c3db601fe8bb62988977a8b62befa8
Tobias <tobz0r7@gmail.com>
2020-03-29 00:05:11 +0100

    Update day1.py

--- 1/day1.py
+++ 1/day1.py
@@ -4,7 +4,6 @@ def read_input(filename):
 	with open('input.txt','r') as input:
 		input_str = input.read().split('\n')
 	data = [int(i) for i in input_str]
-	print("hej")
 	return data
 
 def part_1(data):
